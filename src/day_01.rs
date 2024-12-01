aa9eb1b07a2708b5aec25bfd6bab913b6263ccb1
Charles Lechasseur <shiftingbeard@outlook.com>
2024-12-01 00:17:03 -0500

    Day 1

--- src/day_01.rs
+++ src/day_01.rs
@@ -1,3 +1,30 @@
-pub fn part_1() -> usize {
-    0
+use itertools::Itertools;
+use num::abs;
+
+use crate::input::day_01::INPUT;
+
+pub fn part_1() -> i64 {
+    list(un).zip(list(deux)).map(|(a, b)| abs(a - b)).sum()
+}
+
+pub fn part_2() -> i64 {
+    let counts = list(deux).counts();
+    list(un)
+        .map(|n| n * (counts.get(&n).copied().unwrap_or(0) as i64))
+        .sum()
+}
+
+fn list<F>(f: F) -> impl Iterator<Item = i64>
+where
+    F: Fn(&(i64, i64)) -> i64,
+{
+    INPUT.iter().map(f).sorted_unstable()
+}
+
+fn un(value: &(i64, i64)) -> i64 {
+    value.0
+}
+
+fn deux(value: &(i64, i64)) -> i64 {
+    value.1
 }
--- tests/days/day_01.rs
+++ tests/days/day_01.rs
@@ -1,6 +1,11 @@
-use adventofcode2024_clp::day_01::part_1;
+use adventofcode2024_clp::day_01::{part_1, part_2};
 
 #[test]
 fn day_01_part_1() {
-    assert_eq!(0, part_1());
+    assert_eq!(1_258_579, part_1());
+}
+
+#[test]
+fn day_01_part_2() {
+    assert_eq!(23_981_443, part_2());
 }
