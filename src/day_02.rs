30c46870b46beb58cdc0b9ca9237cfe19af784f2
Charles Lechasseur <shiftingbeard@outlook.com>
2024-12-02 02:01:41 -0500

    Remove example input for day 2

--- src/day_02.rs
+++ src/day_02.rs
@@ -17,17 +17,6 @@ fn input() -> Vec<Vec<i64>> {
     safe_get_input_as_vecs(2024, 2)
 }
 
-fn example_input() -> Vec<Vec<i64>> {
-    vec![
-        vec![7, 6, 4, 2, 1],
-        vec![1, 2, 7, 8, 9],
-        vec![9, 7, 6, 2, 1],
-        vec![1, 3, 2, 4, 5],
-        vec![8, 6, 4, 4, 1],
-        vec![1, 3, 6, 7, 9],
-    ]
-}
-
 #[allow(clippy::ptr_arg)]
 fn safe(levels: &Vec<i64>) -> bool {
     let signum = (levels[0] - levels[1]).signum();
